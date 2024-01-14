da69f44c3ff27f60daa81212ed62d40da13ed0a0
briankim <brianhkim13@gmail.com>
2024-01-13 21:23:37 -0600

    finished product

--- app.rb
+++ app.rb
@@ -5,9 +5,16 @@ water_nymph = FantasyCreatureGenerator.new("Nymph", "Water")
 invisible_spirit = FantasyCreatureGenerator.new("Spirit", "Air")
 golem = FantasyCreatureGenerator.new("Golem", "Earth")
 brian = FantasyCreatureGenerator.new("Brian", "Focusing")
+print "Input a creature type: "
+creature_type = gets.chomp.to_s
+print "Input an element: "
+element = gets.chomp.to_s.downcase.capitalize
+user_creature = FantasyCreatureGenerator.new(creature_type, element)
+
 
 fire_dragon.describe_creature
 water_nymph.describe_creature
 invisible_spirit.describe_creature
 golem.describe_creature
 brian.describe_creature
+user_creature.describe_creature
