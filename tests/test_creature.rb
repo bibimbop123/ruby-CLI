2fa1fcc52547b0e88e20f12eb77bcc85abb1ef3d
briankim <brianhkim13@gmail.com>
2024-01-22 10:23:49 -0600

    still having a hard time with tests

--- tests/test_creature.rb
+++ tests/test_creature.rb
@@ -3,15 +3,14 @@ require './creature'
 
 class TestCreature < Minitest::Test
 
-  def test_initialize
-    @creature_type = FantasyCreatureGenerator.new
-    @element = FantasyCreatureGenerator.new
-    @special_ability = FantasyCreatureGenerator.new
-    @food = FantasyCreatureGenerator.new
+  def test_initialize(creature_type, element)
+    @creature_type = creature_type
+    @element = element
+    @special_ability = test_generate_special_ability(creature_type, element)
+    @food = test_generate_food(creature_type, element)
   end
 
-  def test_generate_special_ability
-    special_ability =FantasyCreatureGenerator.new
+  def test_generate_special_ability(creature_type, element)
     assert_equal case @element
   when "Fire"
     "Breathes scorching flames"
@@ -23,15 +22,14 @@ class TestCreature < Minitest::Test
     "Can manipulate the earth"
   when "Friendship"
     puts "What is Brian's special ability?"
-    gets.chomp.to_s
+   @special_ability = gets.chomp.to_s
   else
     puts "What is this creature's special ability?"
-    gets.chomp.to_s
+   @special_ability = gets.chomp.to_s
   end
 end
 
-def test_generate_food
-  food = FantasyCreatureGenerator.new
+def test_generate_food(creature_type, element)
   assert_equal  case @element
 when "Fire"
   "flesh and bones"
@@ -46,12 +44,11 @@ when "Friendship"
   @food = gets.chomp.to_s
 else
   puts "What does this creature eat?"
-  gets.chomp.to_s
+  @food = gets.chomp.to_s
 end
 end
 
-  def test_describe_creature
-    creature = FantasyCreatureGenerator.new
+  def test_describe_creature(creature_type, element)
     assert_equal "#{creature.creature_type} with an affinity for #{creature.element} loves #{creature.food}. Special ability: #{creature.special_ability}", creature.describe_creature('cactus', 'earth')
   end
 end
